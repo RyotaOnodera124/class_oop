b58dde60f76bd8afc71d72c628e9cf8680c65a44
RyotaOnodera124 <onoryo.0124@gmail.com>
2023-09-29 22:08:22 +0900

    d-6完了

---
+++ d-6.py
@@ -0,0 +1,34 @@
+class MyCounterV3:
+    def __init__(self, value=0, step=1):
+        self.value = value
+        self.step = step
+
+    def count_up(self):
+        self.value += self.step
+
+    def count_down(self):
+        self.value -= self.step
+
+
+# 初期値1でステップ2のカウンターを作成
+counter1 = MyCounterV3(value=1, step=2)
+print(counter1.value)  # 1
+
+counter1.count_up()
+print(counter1.value)  # 3
+
+counter1.count_up()
+print(counter1.value)  # 5
+
+counter1.count_down()
+print(counter1.value)  # 3
+
+# 初期値3でステップ4のカウンターを作成
+counter2 = MyCounterV3(value=3, step=4)
+print(counter2.value)  # 3
+
+counter2.count_down()
+print(counter2.value)  # -1
+
+counter2.count_down()
+print(counter2.value)  # -5
